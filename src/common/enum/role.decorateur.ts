e88a06cbcfe7143a9e68157f2e2fa4ae5fef5d86
emile 4262 <bnandoemile@gmail.com>
2026-09-09 15:20:39 +0000

    fix: le module order et enum

--- src/common/enum/role.decorateur.ts
+++ src/common/enum/role.decorateur.ts
@@ -10,3 +10,9 @@ export enum UserRole {
 
 }
 
+export enum OrderStatus {
+  PENDING = 'PENDING',
+  APPROVED = 'APPROVED',
+  REJECTED = 'REJECTED',
+}
+
---
+++ src/features/orders/dto/create-order.dto.ts
@@ -0,0 +1,14 @@
+// import { ApiProperty } from "@nestjs/swagger";
+// import { IsString } from "class-validator";
+
+// export class CreateOrderDto {
+
+//     @ApiProperty({ example: 'le nom du categories ', description: 'entrez la categories' })
+//     @IsString()
+//     name : string
+
+//     productId: string;
+
+//     quantity: number;
+
+// }
